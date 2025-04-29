d62816253ad67979914491ef172c4e6f33be33db
Gautham-2003 <2003devulapally@gmail.com>
2025-04-29 20:54:22 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,47 @@
+A= 10
+B= 20
+C=print(A+B)
+
+A= 10
+B= 20
+C=print(A-B)
+
+A= 10
+B= 20
+C=print(A*B)
+
+A= 10
+B= 20
+C=print(A/B)
+
+A= 10
+B= 20
+C=print(A//B)
+
+A= 10
+B= 20
+C=print(A%B)
+
+A= 10
+B= 20
+C=print(A>B)
+
+A= 10
+B= 20
+C=print(A<B)
+
+A= 10
+B= 20
+C=print(A>=B)
+
+A= 10
+B= 20
+C=print(A<=B)
+
+A= 10
+B= 20
+C=print(A!=B)
+
+A= 10
+B= 20
+C=print(A==B)
