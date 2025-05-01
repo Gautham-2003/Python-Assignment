41c6b587e875fc6dc47e60dc6b27f4b975fd8911
Gautham-2003 <2003devulapally@gmail.com>
2025-05-01 21:10:56 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,52 @@
+
+#1.Arthimetic Operators
+A=int(input())
+B=int(input())
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+
+#2.Increment and Decrement Operators
+A=int(input())
+C=A+20
+print(C)
+B=A-20
+print(B)
+
+#3.Equal or Not
+A=int(input())
+B=int(input())
+if(A==B):
+    print('Eqaul')
+else:
+    print('Not Equal')
+
+#4.Relation Operators
+A=int(input())
+B=int(input())
+if(A<B):
+    print('True')
+else:
+    print('False')
+if(A<=B):
+    print('True')
+else:
+    print('False')
+if(A>B):
+    print('True')
+else:
+    print('False')
+if(A>=B):
+    print('True')
+else:
+    print('False')
+
+#5.Smaller and Larger Number
+A=int(input())
+B=int(input())
+if(A>B):
+    print('A is Greater')
+else:
+    print('B is Greater')
+
