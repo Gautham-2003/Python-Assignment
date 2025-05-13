c06b35af0b5c348b4d70138d2cd5bc95bc91f5f4
Gautham-2003 <2003devulapally@gmail.com>
2025-05-13 13:20:07 +0530

    Add files via upload

---
+++ Dictionary Assignment.py
@@ -0,0 +1,27 @@
+'''
+Student_Data : 
+    {12345: "P.Lakshmi Prasanna Madam ",
+     67890: "B.Satya Abhinaya Madam ",
+     78901: "K.Avinash Sir",
+     23456: "Siva Sir",
+     78910: "Devulapally Gautham Sathvik"}
+'''
+'''
+A={'a1':10,'a2':20,'a3':30}
+total=sum(A.values())
+print("Sum of Values:",total)
+'''
+'''
+A={'a':10,'b':2,'c': 30}
+A['b']=20
+print(A)
+'''
+'''
+A = {'name': 'Gautham', 'age': 22, 'city': 'Hyderabad(BHARATH)'}
+print(A.keys())
+'''
+'''
+A = {'name': 'Gautham', 'age': 22, 'city': 'Hyderabad(BHARATH)'}
+del A['age']
+print(A)
+'''
