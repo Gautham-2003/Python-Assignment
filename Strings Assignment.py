89e5c2e3cd1080199b3f3f3ee3c0dd5b562649f6
Gautham-2003 <2003devulapally@gmail.com>
2025-05-14 13:20:10 +0530

    Add files via upload

---
+++ Strings Assignment.py
@@ -0,0 +1,37 @@
+'''
+A="I am"
+B="Gautham"
+C=A + " " + B
+print(C)
+'''
+'''
+A="I am Gautham"
+Length = len(A)
+print(Length)
+'''
+'''
+A="Hello"
+B="HELLO"
+print(A==B)
+'''
+'''
+A="Hello, World!"
+A= A.replace("World","Gautham")
+print(A)
+'''
+'''
+A="I am Gautham"
+A=A.split()
+print(A)
+'''
+'''
+A="i am gautham"
+A=A.upper()
+print(A)
+'''
+'''
+A="I AM GAUTHAM"
+A=A.lower()
+print(A)
+'''
+
